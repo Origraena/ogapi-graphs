576f39a467ad76efb0bdc8aa8a2e91f44f3fbdfc
Swan Rocher <swan.rocher@gmail.com>
2012-07-17 10:30:08 +0200

    misc

--- src/ori/ogapi/util/Misc.java
+++ src/ori/ogapi/util/Misc.java
@@ -11,6 +11,15 @@ public class Misc {
 		return value;
 	}
 
+	public static final long max(long array[]) {
+		long value = Long.MIN_VALUE;
+		for (long i : array) {
+			if (value < i)
+				value = i;
+		}
+		return value;
+	}
+
 	public static final float max(float array[]) {
 		float value = Float.MIN_VALUE;
 		for (float i : array) {
@@ -29,15 +38,44 @@ public class Misc {
 		return value;
 	}
 
-	public static final long max(long array[]) {
-		long value = Integer.MIN_VALUE;
-		for (long i : array) {
-			if (value < i)
+	public static final int min(int array[]) {
+		int value = Integer.MAX_VALUE;
+		for (int i : array) {
+			if (value > i)
 				value = i;
 		}
 		return value;
 	}
 
+	public static final long min(long array[]) {
+		long value = Long.MAX_VALUE;
+		for (long i : array) {
+			if (value > i)
+				value = i;
+		}
+		return value;
+	}
+
+	public static final float min(float array[]) {
+		float value = Float.MAX_VALUE;
+		for (float i : array) {
+			if (value > i)
+				value = i;
+		}
+		return value;
+	}
+
+	public static final double min(double array[]) {
+		double value = Double.MAX_VALUE;
+		for (double i : array) {
+			if (value > i)
+				value = i;
+		}
+		return value;
+	}
+
+
+	/** This class cannot be instantiated. */
 	private Misc() { }
 };
 
